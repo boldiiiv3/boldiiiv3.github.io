096961016ebf3385e1e9c4a35e4038d8558b39ad
Boldiii <domjanboldizsar2011@gmail.com>
2025-06-25 19:46:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-$(document).ready(function() {
-  showPolygons();
-  setInterval(function(){
-    showPolygons();
-  },2500)
-})
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
 
-function showPolygons() {
+function animate() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
   
-  $('[class="processed"]').removeAttr('class');
-  var polyCount = $('polygon').length;
-  
-  $('polygon').each(function(ind, el) {
-    setTimeout(function() {
-       $('polygon:eq(' + ind + ')').attr('class', 'processed');
-    }, Math.random() * 2000);
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('img').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
   });
+
+  window.requestAnimationFrame(animate);
 }
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+animate();
